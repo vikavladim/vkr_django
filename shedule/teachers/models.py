610ddae483157d73ab384e57526e63c00bbc14a2
Dmitry Koba <55078980+Tvix57@users.noreply.github.com>
2024-03-18 15:58:51 +0700

    first

---
+++ shedule/teachers/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Teacher(models.Model):
+    fio = models.TextField()
+    photo=models.ImageField(upload_to='teachers/', null=True)
+    room = models.IntegerField()
+
+class Class(models.Model):
+    digit = models.IntegerField()
+    letter = models.CharField(max_length=1)
+
+# class Timing(models.Model):
+#     class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+#     lesson_number = models.IntegerField()
+#     start_time = models.TimeField()
+#     end_time = models.TimeField()
+
+class Discipline(models.Model):
+    name = models.TextField()
+
+class Subgroup(models.Model):
+    class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+    group_number = models.IntegerField()
+    discipline_id = models.ForeignKey(Discipline, on_delete=models.CASCADE)
+    teacher_id = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+class Program(models.Model):
+    class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+    discipline_id = models.ForeignKey(Discipline, on_delete=models.CASCADE)
+    load = models.IntegerField()
---
+++ shedule/teachers/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
+
+
+def create(request):
+    return HttpResponse('creating teacher')
+
+
+def read(request, id):
+    if id == 0:
+        raise Http404
+    elif id > 10:
+        return redirect('home', permanent=True)
+    return HttpResponse(f'reading teacher {id}')
+
+
+def update(request, id):
+    return HttpResponse(f'updating teacher {id}')
+
+
+def delete(request, id):
+    return HttpResponse(f'deleting teacher {id}')
+
+
+def page_not_found(request, exception):
+    print('ggggggggggggg')
+    return HttpResponseNotFound('Page not found', status=404)
+
+def all(request):
+    return HttpResponse('all teachers')
